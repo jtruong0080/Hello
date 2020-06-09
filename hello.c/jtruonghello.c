b28bf5abbde9d69c7aa96ec1cf50b47bc18005cc
salegrettm <66648909+salegrettm@users.noreply.github.com>
2020-06-09 10:49:14 -0500

    Sebastian Major added
    Major added and changes to code

---
+++ hello.c/jtruonghello.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Judy Truong\n");
+  printf("Math\n");
+  printf("Sebastian Alegrett\n");
+  printf("CompSci\n");
+  return 0;
+}
